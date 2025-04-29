5579806e8b7905d0cdfdf21d526d234d4dbd3ab6
haster12345 <tarunmankani25@gmail.com>
2025-04-29 23:52:29 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,15 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            print(data)
+            conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+
+
 
 
 if __name__ == "__main__":
