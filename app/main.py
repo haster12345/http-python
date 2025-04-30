c51d7fe328dbf3b756a6a20a069c6e8984f693e6
haster12345 <tarunmankani25@gmail.com>
2025-04-30 17:21:35 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,30 @@
 import socket  # noqa: F401
+from typing import Any
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept()
+    conn: socket
+    addr: tuple[str, str]
+
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.recv(1024)
-            print(data)
-            conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+            data: bytes = conn.recv(1024)
+            decoded_data: str = data.decode("utf-8")
+            res = parser(decoded_data)
+            if res and res[0] == "GET":
+                url_path = res[1]
+                print(url_path)
+                if url_path == "/":
+                    conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+                else:
+                    conn.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
 
-
+def parser(data: str) -> list[str]:
+    return data.split()
 
 
 if __name__ == "__main__":
